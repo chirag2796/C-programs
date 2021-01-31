79351ff0df9bbebccb3b2271e038a3695b20fbc5
unknown <chirag2796@gmail.com>
2021-01-31 14:06:05 +0530

    Initial Commit

---
+++ bisection/main.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+#define F(x) exp(2*x)*cos(3*x)
+#define G(x) exp(2*x)*(2*cos(3*x)-3*sin(3*x))
+void main()
+{
+  float x[10],y[10],temp=1,f[10],sum,p;
+  int i,n,j,k=0,c;
+  printf("\nEnter the degree of polynomial");
+  scanf("%d",&n);
+  for(i=0; i<n; i++)
+  {
+   printf("\n\nenter the value of x%d: ",i);
+   scanf("%f",&x[i]);
+   y[i]=F(x[i]);
+  }
+  printf("\n\nEnter X for finding f(x): ");
+  scanf("%f",&p);
+
+  for(i=0;i<n;i++)
+  {
+    temp = 1;
+    k = i;
+    for(j=0;j<n;j++)
+    {
+      if(k==j)
+      {
+        continue;
+      }
+      else
+      {
+        temp = temp * ((p-x[j])/(x[k]-x[j]));
+      }
+    }
+    f[i]=y[i]*temp;
+  }
+
+  for(i=0;i<n;i++)
+  {
+     sum = sum + f[i];
+  }
+
+  float a=1,b=1,d=fabs(G(x[0])),e,h;
+  for(i=0;i<n;i++){
+    a*=fabs((p-x[i]));
+    b*=(i+1);
+  }
+  for(h=x[0];h<=x[n-1];h+=0.001){
+    if(fabs(G(h))>d){
+        d=fabs(G(h));
+    }
+  }
+
+  e=(a*d)/b;
+
+  printf("\n\n f(%.1f) = %f ",p,sum);
+  printf("\n\n error bound %f ",e);
+  getch();
+}
